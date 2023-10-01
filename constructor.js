3b7c3fc81d328a71d3c393214ee5e17a19ca7b33
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-10-01 13:16:38 +1100

    add preference save/load

--- constructor.js
+++ constructor.js
@@ -1,11 +1,43 @@
-
 class GMConstructor {
 
-    static plugin_name = 'constructor';
+    static plugin_name = 'GMEdit-Constructor';
     static version = '0.1.0';
+
+    static preferences_path = `${Electron_App.getPath('userData')}/GMEdit/config/constructor-preferences.json`;
+    static preferences = {
+        
+    };
+
+    static loadPreferences = () => {
+        if (!Electron_FS.existsSync(this.preferences_path)) {
+            return this.savePreferences();
+        }
+
+        try {
+            const prefs_str = Electron_FS.readFileSync(this.preferences_path);
+            Object.assign(this.preferences, JSON.parse(prefs_str));
+        } catch (err) {
+            this.showError(`Failed to load preferences:\n${err}\n\nUsing defaults.`);
+        }
+    }
+
+    static savePreferences = () => {
+        try {
+            Electron_FS.writeFileSync(this.preferences_path, JSON.stringify(this.preferences));
+        } catch (err) {
+            this.showError(`Failed to write preferences:\n${err}`);
+        }
+    }
+
+    /**
+     * @param {string|Error} error
+     */
+    static showError = (error) => {
+        console.log(`${this.plugin_name}: ${error}`);
+    }
     
     static init = () => {
-
+        this.loadPreferences();
     }
 
     static cleanup = () => {
--- global.d.ts
+++ global.d.ts
@@ -7,3 +7,13 @@ declare type GMPlugin = {
 declare class GMEdit {
     static register = function(name: string, body: GMPlugin) {}
 }
+
+declare class Electron_App {
+    static getPath = function(name: string): string {}
+}
+
+declare class Electron_FS {
+    static readFileSync = function(path: string): string {}
+    static writeFileSync = function(path: string, content: string) {}
+    static existsSync = function(path: string): boolean {}
+}
