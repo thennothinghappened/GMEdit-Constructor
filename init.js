fd88acd48e626434355245cb134c9374f737d7a6
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-11-16 13:07:38 +1100

    Increment version

--- init.js
+++ init.js
@@ -5,7 +5,7 @@
 (() => {
 
     const plugin_name = 'GMEdit-Constructor';
-    const plugin_version = '0.5.1';
+    const plugin_version = '0.6.0';
 
     const node_child_process = require('node:child_process');
     const node_path = require('node:path');
