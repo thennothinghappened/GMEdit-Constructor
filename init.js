695ab607b954f7b5e5e265194e373c688b84c279
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-05-03 11:04:54 +1000

    feat: Release `0.10.1`

--- init.js
+++ init.js
@@ -5,7 +5,7 @@
 (() => {
 
     const plugin_name = 'GMEdit-Constructor';
-    const plugin_version = '0.10.0';
+    const plugin_version = '0.10.1';
 
     const node_child_process = require('node:child_process');
     const node_path = require('node:path');
