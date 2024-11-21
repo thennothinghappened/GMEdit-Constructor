a75b16c6c9528a463a57c130970f004073fb8b89
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-11-21 22:45:57 +1100

    fix: regex account for numbers in event names now
    For whatever reason new runtimes now display the numerical index of an event which was previously only used internally. I have no idea why this is, nor why you'd want this, but alas, it is now supported.

--- js/compiler/job/JobError.js
+++ js/compiler/job/JobError.js
@@ -183,7 +183,7 @@ export class JobRunnerError extends JobError {
 	 * Some silly regex that captures an error in either the 2024.400<= or 2024.600+ format, since
 	 * they randomly changed it ever-so-slightly.
 	 */
-	static regex = /ERROR!!! :: #+?\nERROR in\saction number 1\sof (?<event>[A-Za-z ]+?)\sfor object (?<object>\S+?):\n+(?<exception>[\s\S]+?)\n#+?\n(?<script>gml_\S+?) \(line (?<line_number>[0-9]+?)\)/;
+	static regex = /ERROR!!! :: #+?\nERROR in\saction number 1\sof (?<event>[A-Za-z0-9 ]+?)\sfor object (?<object>\S+?):\n+(?<exception>[\s\S]+?)\n#+?\n(?<script>gml_\S+?) \(line (?<line_number>[0-9]+?)\)/;
 
     /**
      * @param {String} object 
