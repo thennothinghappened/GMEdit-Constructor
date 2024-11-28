e6f75a3b49e9fbe49545f9e4096f1706290bef9d
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-11-28 18:02:33 +1100

    fix(types): `undefined` slipping thru in `Wrapper`

--- js/utils/scope-extensions/index.d.ts
+++ js/utils/scope-extensions/index.d.ts
@@ -2,5 +2,5 @@ import { Wrapper } from './use';
 
 export declare global {
 	type NonUndefined<T> = T extends undefined ? never : T;
-	type WrapperIfNonUndefined<R> = R extends undefined ? undefined : Wrapper<R>;
+	type WrapperIfNonUndefined<R> = R extends undefined ? undefined : Wrapper<NonUndefined<R>>;
 };
