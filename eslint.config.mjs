0d62f6f5e7ab38b31e937a2034b6529e859d8d80
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2025-01-05 07:49:20 +1100

    chore: add eslint to also yell at me
    heck, why not. mainly just for cleanliness and to tell me about random bad stuff and unused things

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{
+		files: ["**/*.{js,ts}"]
+	},
+	{
+		languageOptions: { 
+			globals: globals.browser 
+		}
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		rules: {
+			'no-undef': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_'
+				}
+			],
+			'@typescript-eslint/ban-ts-comment': [
+				'warn',
+				{
+					'ts-nocheck': false
+				}
+			]
+		}
+	},
+	{
+		files: ['init.js'],
+		rules: {
+			'@typescript-eslint/no-require-imports': 'off'
+		}
+	}
+];
