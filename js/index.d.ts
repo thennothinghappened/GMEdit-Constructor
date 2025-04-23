a75bc130c7694d12fdc7a6daee479ea3c13bea4d
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2025-04-23 11:17:22 +1000

    refactor(types): some stuff wrongly classed as UI types

--- js/index.d.ts
+++ js/index.d.ts
@@ -2,6 +2,7 @@ import { ConstructorPlugin } from './ConstructorPlugin';
 import { ProjectProperties } from './preferences/ProjectProperties';
 import { ConfigTreeUi } from './ui/ConfigTreeUi';
 import { ProjectPropertiesMenu } from './ui/ProjectPropertiesMenu';
+import { BaseError } from './utils/Err';
 
 export declare global {
 
@@ -21,6 +22,47 @@ export declare global {
 	};
 
 	/**
+	 * A method of reporting errors to users.
+	 * 
+	 * The methods here are to be used in cases where the user needs to be informed of some error,
+	 * generally configuration-related and requiring their input, or to report an illegal condition
+	 * which the plugin does not know how to resolve.
+	 * 
+	 * Ideally these methods' usage be minimal, and should generally be avoided at lower levels, in
+	 * favour of sending errors upwards for handling. The top-level logic then can make a decision
+	 * as to what should happen, which may then be to report it to the user as here, or some other
+	 * action.
+	 */
+	interface ProblemLogger {
+
+		/**
+		 * Write an error message in the UI. Error messages implicitly show the problems pane
+		 * immediately.
+		 * 
+		 * @param title
+		 * @param err
+		 */
+		error(title: string, err: BaseError): ProblemLogger;
+
+		/**
+		 * Write a warning message in the UI.
+		 * 
+		 * @param title
+		 * @param err
+		 */
+		warn(title: string, err: BaseError): ProblemLogger;
+
+		/**
+		 * Write a debug message in the UI.
+		 * 
+		 * @param title
+		 * @param err
+		 */
+		debug(title: string, err: BaseError): ProblemLogger;
+
+	};
+
+	/**
 	 * Components held by the plugin singleton for the currently open project.
 	 */
 	type ProjectComponents = {
@@ -33,9 +75,16 @@ export declare global {
 		configTreeUi: ConfigTreeUi;
 	};
 
-	type UIGroup = { 
-		legend: HTMLLegendElement;
-	} & HTMLFieldSetElement;
+	/**
+	 * Global actions that may be ran by the user.
+	 */
+	interface PluginCommands {
+		showControlPanel(): void;
+		stopCurrentProject(): void;
+		runCurrentProject(): void;
+		cleanCurrentProject(): void;
+		packageCurrentProject(): void;
+	};
 
 	interface Window {
 		ConstructorPlugin?: ConstructorPlugin;
--- js/ui/index.d.ts
+++ js/ui/index.d.ts
@@ -1,57 +1,8 @@
-import { BaseError } from "../utils/Err";
 
 export declare global {
 
-	/**
-	 * A method of reporting errors to users.
-	 * 
-	 * The methods here are to be used in cases where the user needs to be informed of some error,
-	 * generally configuration-related and requiring their input, or to report an illegal condition
-	 * which the plugin does not know how to resolve.
-	 * 
-	 * Ideally these methods' usage be minimal, and should generally be avoided at lower levels, in
-	 * favour of sending errors upwards for handling. The top-level logic then can make a decision
-	 * as to what should happen, which may then be to report it to the user as here, or some other
-	 * action.
-	 */
-	interface ProblemLogger {
-
-		/**
-		 * Write an error message in the UI. Error messages implicitly show the problems pane
-		 * immediately.
-		 * 
-		 * @param title
-		 * @param err
-		 */
-		error(title: string, err: BaseError): ProblemLogger;
-
-		/**
-		 * Write a warning message in the UI.
-		 * 
-		 * @param title
-		 * @param err
-		 */
-		warn(title: string, err: BaseError): ProblemLogger;
-
-		/**
-		 * Write a debug message in the UI.
-		 * 
-		 * @param title
-		 * @param err
-		 */
-		debug(title: string, err: BaseError): ProblemLogger;
-
-	};
-
-	/**
-	 * Global actions that may be ran by the user.
-	 */
-	interface PluginCommands {
-		showControlPanel(): void;
-		stopCurrentProject(): void;
-		runCurrentProject(): void;
-		cleanCurrentProject(): void;
-		packageCurrentProject(): void;
+	type UIGroup = HTMLFieldSetElement & {
+		legend: HTMLLegendElement;
 	};
 
 }
